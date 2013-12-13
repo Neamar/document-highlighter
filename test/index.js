ea44ecadbc35deb18c214b5ac3bb55ed0e6ab55a
Neamar <neamar@neamar.fr>
2013-12-13 11:40:08 +0100

    Moar tests

--- test/index.js
+++ test/index.js
@@ -4,10 +4,15 @@ require('should');
 var documentHighlight = require('../lib');
 
 var generateIts = function(its) {
+  var defaultOptions = {
+    before: '*',
+    after: '*'
+  };
+
   for(var itShould in its) {
     var itDatas = its[itShould];
     it(itShould, function() {
-      documentHighlight(itDatas[0], itDatas[1]).should.eql(itDatas[2]);
+      documentHighlight(itDatas.text, itDatas.query, defaultOptions).should.eql(itDatas.expected);
     });
   }
 };
@@ -15,7 +20,46 @@ var generateIts = function(its) {
 describe('Standard mode', function() {
   describe('with text content', function() {
     var its = {
-      'should not modify non matching text': ['Some text', 'non matching query', 'Some text']
+      'should not modify non-matching text': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'non matching query',
+        expected: 'Hello and welcome to the real world, Neo'
+      },
+      'should highlight relevant text': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'welcome to the real world',
+        expected: 'Hello and *welcome to the real world*, Neo',
+      },
+      'should be case insensitive to the text': {
+        text: 'Hello and WELCOME to the real world, Neo',
+        query: 'welcome to the real world',
+        expected: 'Hello and *WELCOME to the real world*, Neo',
+      },
+      'should be case insensitive to the query': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'WELCOME to the real world',
+        expected: 'Hello and *welcome to the real world*, Neo',
+      },
+      'should use unicode mapping': {
+        text: 'Hello and wélcöme to the real world, Neo',
+        query: 'welcome to the real world',
+        expected: 'Hello and *wélcöme to the real world*, Neo',
+      },
+      'should match standard lexemes': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'welcome to the reals worlds',
+        expected: 'Hello and *welcome to the real world*, Neo',
+      },
+      'should split non contiguous queries': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'hello world',
+        expected: '*Hello* and welcome to the real *world*, Neo',
+      },
+      'should include stop-words queries': {
+        text: 'Hello and welcome to the real world, Neo',
+        query: 'welcome real world',
+        expected: 'Hello and *welcome to the real world*, Neo',
+      },
     };
 
     generateIts(its);
