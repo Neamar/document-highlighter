eab35b53b22ee73430582f8618641d29285b12ab
Neamar <neamar@neamar.fr>
2013-12-18 11:28:49 +0100

    All non edge-cases tests passing

--- test/index.js
+++ test/index.js
@@ -5,24 +5,24 @@ var documentHighlight = require('../lib');
 
 describe('Highlight options', function() {
   it('should not be mandatory', function() {
-    documentHighlight("my text", "text").should.eql("my <strong>text</strong>");
+    documentHighlight.text("my text", "text").text.should.eql("my <strong>text</strong>");
   });
 
   it('should allow override of before and after', function() {
-    documentHighlight("my text", "text", {before: '^', after: '$'}).should.eql("my ^text$");
+    documentHighlight.text("my text", "text", {before: '^', after: '$'}).text.should.eql("my ^text$");
   });
 
   it('should load language datas', function() {
     // No match in en
-    documentHighlight("my téxt", "text", {language: "en"}).should.eql("my téxt");
+    documentHighlight.text("my téxt", "text", {language: "en"}).text.should.eql("my téxt");
 
     // Match in fr
-    documentHighlight("my téxt", "text", {language: "fr"}).should.eql("my <strong>téxt</strong>");
+    documentHighlight.text("my téxt", "text", {language: "fr"}).text.should.eql("my <strong>téxt</strong>");
   });
 
   it('should forbid unknown languages', function() {
     try {
-      documentHighlight("my text", "text", {language: "nope"});
+      documentHighlight.text("my text", "text", {language: "nope"});
     } catch(e) {
       return;
     }
